636042be1785488706fb6539c10052cfa2318bab
kkbo5222 <kkbo5222@gmail.com>
2022-06-14 15:13:52 +0900

    5.2.1 연결 리스트 - 이중 연결 리스트

--- 5.1 복잡도/5.2.1 연결 리스트.py
+++ 5.1 복잡도/5.2.1 연결 리스트.py
@@ -104,6 +104,110 @@ class SingleLinkedList:
         return str(self.list_size)
 
 
+# 이중 연결 리스트
+class DualLinkedList:
+    def __init__(self, data):
+        new_node = Node(data)
+        self.head = new_node
+        self.list_size = 1
+
+    def __str__(self):
+        print_list = '[ '
+        node = self.head
+        while True:
+            print_list += str(node)
+            if node.next == self.head:
+                break
+            node = node.next
+            print_list += ', '
+        print_list += ' ]'
+        return print_list
+
+    def insertFirst(self, data):
+        new_node = Node(data)
+        if not self.head.prev == None:
+            new_node.prev = self.head.prev
+            self.head.prev.next = new_node
+        if not self.head.next == None:
+            new_node.next = self.head.next
+        self.head.prev = new_node
+        self.head = new_node
+
+    def insertMiddleAfter(self, num, data):
+        node = self.selectNode(num)
+        new_node = Node(data)
+        new_node.prev = node
+        new_node.next = node.next
+        node.next.prev = new_node
+        node.next = new_node
+        self.list_size += 1
+
+    def insertMiddleBefore(self, num, data):
+        node = self.selectNode(num)
+        new_node = Node(data)
+        new_node.prev = node.prev
+        new_node.next = node
+        node.prev.next = new_node
+        node.prev = new_node
+        self.list_size += 1
+
+    def insertLast(self, data):
+        new_node = Node(data)
+        if self.head.next == None:
+            self.head.next = new_node
+            new_node.prev = self.head
+
+        if not self.head.prev == None:
+            self.head.prev.next = new_node
+            new_node.prev = self.head.prev
+        self.head.prev = new_node
+        new_node.next = self.head
+        self.list_size += 1
+
+    def selectNode(self, num):
+        if self.list_size < 1:
+            return # Underflow
+        elif self.list_size <= num:
+            return # Overflow
+        count = 0
+        node = self.head
+        if int(self.list_size/2) > num:
+            while count < num:
+                node = node.next
+                count += 1
+        else:
+            repeat = self.list_size - num
+            while count < repeat:
+                node = node.prev
+                count += 1
+        return node
+
+    def deleteNode(self, num):
+        if self.list_size < 1:
+            return # Underflow
+        elif self.list_size <= num:
+            return # Overflow
+
+        if num == 0:
+            self.deleteHead()
+            return
+        node = self.selectNode(num)
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        del node
+
+    def deleteHead(self):
+        node = self.head
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        self.head = node.next
+        del node
+
+    def size(self):
+        return str(self.list_size)
+
+
+
 # 원형 연결 리스트
 class CircleLinkedList:
     def __init__(self, data):
